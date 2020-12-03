f438f29c13120e6ad3b88772514fcec5917025f7
qiuyufei <72742566+qiuyufei@users.noreply.github.com>
2020-12-03 13:23:08 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,43 +1,31 @@
 package shiyan5;
+import java.util.Scanner;
 
-public class Student {
-    public String name;
-    public int age;
-    public String sex;
-    public String number;
-    public Student(String name,int age,String sex,String number){
-     this.name=name;
-     this.age=age;
-     this.sex=sex;
-     this.number=number;
-    }
-    public String getName(){
-     return name;
-    }
-    public void setName(String name){
-     this.name=name;
-    }
-    public int getAge(){
-     return age;
-    }
-    public void setAge(int age){
-     this.age=age;
-    }
-    public String getSex(){
-     return sex;
-    }
-    public void setSex(String sex){
-     this.sex=sex;
-    }
-    public String getNumber(){
-     return number;
-    }
-    public void setNumber(){
-     this.number=number;
-    }
-@Override
-public  String toString() {
- return "student [name=" + name + ", age=" + age + ", sex=" + sex + ", number=" + number + "]";
-}
-    
+class Student {
+	public void toshow() {
+		String name;							//学生的姓名	
+		int num;							    //学生的学号
+		String sex;							    //学生的性别
+		int age;								//学生的年龄
+		String grade;							//学生的班级
+		
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("姓名：");
+	    name=sc.next();
+		System.out.println("编号：");
+	    num=sc.nextInt();
+		System.out.println("性别：");
+	    sex=sc.next();
+		System.out.println("年龄");
+	    age=sc.nextInt();
+		System.out.println("班级：");
+	    grade=sc.next();
+
+	   
+	    System.out.println("姓名："+name+"编号："+num+"性别："+sex+"年龄："+age+"班级："+grade);
+	
+		
+	}
+	
 }
